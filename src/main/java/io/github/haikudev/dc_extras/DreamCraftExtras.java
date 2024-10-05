12a5b35b0825551a3e31953b4772db50ff8c951b
hakaiku <117446340+HakaikuXiao@users.noreply.github.com>
2024-10-05 22:48:57 +0600

    Empty project

---
+++ src/main/java/io/github/haikudev/dc_extras/DreamCraftExtras.java
@@ -0,0 +1,10 @@
+package io.github.haikudev.dc_extras;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DreamCraftExtras implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/haikudev/dc_extras/client/DreamCraftExtrasClient.java
@@ -0,0 +1,10 @@
+package io.github.haikudev.dc_extras.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DreamCraftExtrasClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
